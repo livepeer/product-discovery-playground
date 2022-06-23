f19d34c03344dd03546463a693055f4055df9d8e
Eli Mallon <eli@livepeer.org>
2022-06-22 18:11:44 -0700

    worker: add DEFAULT_STREAM handling, lower-casing

--- worker/worker.ts
+++ worker/worker.ts
@@ -20,6 +20,17 @@ function handlePushRewrite(text: string): string {
   return pathname.slice(PATHNAME_PREFIX.length);
 }
 
+// DEFAULT_STREAM payload:
+// current defaultStream setting (string)
+// requested stream name (string)
+// viewer host (string)
+// output type (string)
+// full request URL (string, may be blank for non-URL-based requests!)
+function handleDefaultStream(text: string): string {
+  const streamName = text.split("\n")[1];
+  return `${STREAM_PREFIX}+${streamName}`;
+}
+
 async function handleRequest(request: Request): Promise<Response> {
   try {
     if (request.method !== "POST") {
@@ -30,18 +41,19 @@ async function handleRequest(request: Request): Promise<Response> {
     let b64;
     if (pathname === "/hooks/PUSH_REWRITE") {
       b64 = handlePushRewrite(text);
+    } else if (pathname === "/hooks/DEFAULT_STREAM") {
+      // TODO: .eth and .lens lookups can go in here!
+      const streamName = handleDefaultStream(text);
+      return new Response(streamName);
     } else if (pathname === "/") {
       b64 = text;
     } else {
       return new Response("not found", { status: 404 });
     }
     const data = JSON.parse(atob(b64));
-    const addr = ethers.utils.verifyTypedData(
-      DOMAIN,
-      TYPES,
-      data.message,
-      data.signature
-    );
+    const addr = ethers.utils
+      .verifyTypedData(DOMAIN, TYPES, data.message, data.signature)
+      .toLowerCase();
     return new Response(`${STREAM_PREFIX}+${addr}`);
   } catch (e: any) {
     return new Response(e.message, { status: 500 });
