bb3e510669778293c45494e96dcce960caef6f8e
Rafał Leszko <rafal@livepeer.org>
2023-06-26 14:34:22 +0200

    Refactor to reuse code

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -44,9 +44,20 @@ type DrawerItem = {
   className?: string;
 };
 
-const Layout = ({ children, title = "Livepeer Product Discovery", networkType="eth"}) => {
-  const { pathname, asPath } = useRouter();
+const FlowLogo = () => {
+  return (
+    <div className="App">
+      <svg width="30" height="42" viewBox="0 0 50 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20.8333 41.8334C32.3393 41.8334 41.6667 32.506 41.6667 21.0001C41.6667 9.49415 32.3393 0.166748 20.8333 0.166748C9.3274 0.166748 0 9.49415 0 21.0001C0 32.506 9.3274 41.8334 20.8333 41.8334Z" fill="#00EF8B"></path>
+        <path d="M29.9732 17.7417H24.0898V23.625H29.9732V17.7417Z" fill="white"></path>
+        <path d="M18.2138 25.8292C18.2138 26.266 18.0843 26.6929 17.8416 27.0561C17.599 27.4192 17.2541 27.7023 16.8506 27.8694C16.447 28.0366 16.003 28.0803 15.5746 27.9951C15.1463 27.9099 14.7528 27.6996 14.4439 27.3907C14.1351 27.0819 13.9248 26.6884 13.8396 26.26C13.7544 25.8316 13.7981 25.3876 13.9652 24.9841C14.1324 24.5806 14.4154 24.2357 14.7786 23.993C15.1417 23.7504 15.5687 23.6209 16.0055 23.6209H18.2138V17.7417H16.0055C14.4059 17.7417 12.8423 18.216 11.5123 19.1047C10.1823 19.9934 9.14572 21.2564 8.5336 22.7342C7.92147 24.212 7.76131 25.8382 8.07337 27.407C8.38543 28.9758 9.15569 30.4169 10.2867 31.5479C11.4178 32.679 12.8589 33.4492 14.4277 33.7613C15.9965 34.0733 17.6226 33.9132 19.1004 33.3011C20.5782 32.6889 21.8413 31.6523 22.73 30.3224C23.6186 28.9924 24.093 27.4287 24.093 25.8292V23.6209H18.2138V25.8292Z" fill="white"></path>
+        <path d="M26.2984 14.8001H32.9151V8.91675H26.2984C24.1542 8.91895 22.0984 9.77174 20.5821 11.288C19.0659 12.8042 18.2131 14.86 18.2109 17.0042V17.7417H24.0901V17.0042C24.0912 16.4193 24.3244 15.8587 24.7384 15.4454C25.1524 15.0322 25.7135 14.8001 26.2984 14.8001V14.8001Z" fill="white"></path>
+      </svg>
+    </div>
+  );
+}
 
+const FlowWallet = () => {
   const [user, setUser] = useState({loggedIn: null})
 
   useEffect(() => {fcl.currentUser.subscribe(setUser)}, [])
@@ -81,6 +92,15 @@ const Layout = ({ children, title = "Livepeer Product Discovery", networkType="e
       </div>
     )
   }
+
+  return (user.loggedIn
+    ? <AuthedState />
+    : <UnauthenticatedState />
+  )
+}
+
+const Layout = ({ children, title = "Sign Verifiable Video Attestation", networkType="eth"}) => {
+  const { pathname, asPath } = useRouter();
 
   const mutations = useMutations();
   const activeChain = useActiveChain();
@@ -308,14 +328,17 @@ const Layout = ({ children, title = "Livepeer Product Discovery", networkType="e
                                 }
                               </Box>
                             }
+                            { networkType === "flow" && <FlowLogo/> }
+                            { networkType === "flow" &&
+                              <Box css={{ ml: "8px" }}>
+                                Flow
+                              </Box>
+                            }
+                              
                           </Flex>
                           <Flex css={{ ai: "center", ml: "8px" }}>
                               { networkType === "eth" && <ConnectButton showBalance={false} /> }
-                              { networkType === "flow" && (user.loggedIn
-                                  ? <AuthedState />
-                                  : <UnauthenticatedState />
-                                )
-                              }
+                              { networkType === "flow" && <FlowWallet/> }
                           </Flex>
                         </Flex>
                       </Flex>
---
+++ pages/main.tsx
@@ -0,0 +1,337 @@
+import Layout from "@layouts/main";
+
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from "@livepeer/design-system";
+import { useEffect, useState } from "react";
+import { useAccount, useSignTypedData } from "wagmi";
+import * as fcl from "@onflow/fcl";
+
+import Spinner from "@components/Spinner";
+import { TypedDataField } from "@ethersproject/abstract-signer";
+import { useCreateAsset } from "@livepeer/react";
+import stringify from "fast-stable-stringify";
+
+type VideoAttributes = {
+  video: string;
+  timestamp: number;
+  signer: string;
+  attestations: {
+    role: string;
+    address: string;
+  }[];
+};
+
+type AttestationResponse = {
+  id: string;
+  signatureType: "eip712";
+  createdAt: 1686334888047;
+  primaryType: "VideoAttestation";
+  domain: {
+    name: string;
+    version: string;
+  };
+  message: {
+    video: string;
+    timestamp: number;
+    signer: string;
+    attestations: [
+      {
+        role: "creator";
+        address: string;
+      }
+    ];
+  };
+  signature: string;
+};
+
+const Main = ({networkType}) => {
+  const account = useAccount();
+
+  const [user, setUser] = useState({loggedIn: null})
+  useEffect(() => {fcl.currentUser.subscribe(setUser)}, [])
+
+  const [isUploading, setIsUploading] = useState(false);
+
+  const [errorUpload, setErrorUpload] = useState("");
+  const [attestationId, setAttestationId] = useState("");
+
+  const [fileUpload, setFileUpload] = useState<File | null>(null);
+
+  const {
+    isLoading,
+    data: asset,
+    error,
+    mutate,
+  } = useCreateAsset({
+    sources: fileUpload
+      ? [
+          {
+            file: fileUpload,
+            name: fileUpload.name,
+            storage: {
+              ipfs: true,
+              metadataTemplate: "file",
+            },
+          },
+        ]
+      : [],
+  });
+
+  useEffect(() => {
+    if (fileUpload) {
+      mutate();
+    }
+  }, [fileUpload]);
+
+  const [jsonSchema, setJsonSchema] = useState<object | null>(null);
+  const [signatureTypes, setSignatureTypes] = useState<Record<
+    string,
+    TypedDataField[]
+  > | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const result = await fetch(`/json-schemas/attestation.schema.json`);
+      const json = await result.json();
+
+      setJsonSchema(json);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const result = await fetch(`/json-schemas/attestation.types.json`);
+      const json = await result.json();
+
+      setSignatureTypes(json);
+    })();
+  }, []);
+  const { signTypedDataAsync } = useSignTypedData();
+
+  const isWalletConnected = () => {
+    if (networkType === "eth") {
+      return account?.data?.address
+    } else if (networkType === "flow") {
+      return user.loggedIn;
+    }
+  }
+
+  const address = () => {
+    if (networkType === "eth") {
+      return account.data.address;
+    } else if (networkType === "flow") {
+      return user?.addr;
+    }
+  }
+
+  const sign = async (domain, message) => {
+    if (networkType == "eth") {
+      return await signTypedDataAsync({
+        domain,
+        types: signatureTypes,
+        value: message,
+      });
+    } else if (networkType === "flow") {
+      const signatures = await fcl
+      .currentUser()
+      .signUserMessage(Buffer.from(stringify(message)).toString("hex"));
+      return signatures[0].signature;
+    }
+  }
+
+  const onSubmitMetadata = async () => {
+    setIsUploading(true);
+    setErrorUpload("");
+
+    const domain = {
+      name: "Verifiable Video",
+      version: "1",
+    };
+
+    try {
+      const message: VideoAttributes = {
+        video: asset?.[0]?.storage?.ipfs?.url ?? "",
+        timestamp: Date.now(),
+        signer: address(),
+        attestations: [
+          {
+            role: "creator",
+            address: address(),
+          },
+        ],
+      };
+
+      const signature = await sign(domain, message);
+
+      try {
+        const data = {
+          primaryType: "VideoAttestation",
+          domain,
+          message,
+          signature: signature,
+        };
+
+        const res = await fetch(
+          "https://livepeer.monster/api/experiment/-/attestation",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
+
+        if (res.ok) {
+          const json: AttestationResponse = await res.json();
+
+          if (json.id) {
+            setAttestationId(json.id);
+          }
+        } else {
+          setErrorUpload("Error uploading, please try again.");
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsUploading(false);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  return (
+    <>
+      <Container size="3" css={{ width: "100%" }}>
+        <Flex
+          css={{
+            flexDirection: "column",
+            mt: "$3",
+            width: "100%",
+            "@bp3": {
+              mt: "$6",
+            },
+          }}
+        >
+          <Heading
+            as="h1"
+            css={{
+              mb: "$5",
+              color: "$hiContrast",
+              fontSize: "$3",
+              fontWeight: 600,
+              display: "none",
+              alignItems: "center",
+              "@bp2": {
+                fontSize: "$7",
+              },
+              "@bp3": {
+                display: "flex",
+                fontSize: "$7",
+              },
+            }}
+          >
+            Create a Verified Video
+          </Heading>
+
+          <Box css={{ maxWidth: 600 }}>
+            <Text css={{ mb: "$3" }}>
+              Create a verified video with decentralized storage (IPFS) and play
+              it back with the Livepeer protocol.
+            </Text>
+
+            <Flex
+              css={{
+                gap: "$3",
+                mb: "$3",
+                mt: "$3",
+              }}
+            >
+              <Flex css={{ flexDirection: "column" }}>
+                <Text size="2" css={{ fontWeight: 700 }}>
+                  Upload a video to IPFS
+                </Text>
+
+                {isWalletConnected() && (
+                  <input
+                    type="file"
+                    accept="video/mp4,video/x-m4v,video/*"
+                    style={{ display: "none" }}
+                    id="contained-button-file"
+                    onChange={(e) => {
+                      if (e?.target?.files?.[0]) {
+                        setFileUpload(e.target.files[0]);
+                      }
+                    }}
+                  />
+                )}
+
+                <Button
+                  disabled={
+                    asset?.[0]?.storage?.ipfs?.url ||
+                    !isWalletConnected() ||
+                    isUploading ||
+                    fileUpload
+                  }
+                  variant="primary"
+                  size={2}
+                >
+                  {isLoading && <Spinner />}
+                  <label htmlFor="contained-button-file">Upload Video</label>
+                </Button>
+              </Flex>
+            </Flex>
+
+            {jsonSchema && asset?.[0]?.storage?.ipfs?.url && (
+              <Flex css={{}}>
+                <Button
+                  disabled={!fileUpload || isUploading || attestationId}
+                  variant="primary"
+                  size={2}
+                  onClick={onSubmitMetadata}
+                >
+                  Sign & Create Verifiable Video
+                </Button>
+              </Flex>
+            )}
+
+            <Flex css={{ justifyContent: "flex-end", alignItems: "center" }}>
+              {errorUpload ? (
+                <Text css={{ color: "$red11", mt: "$3" }}>
+                  {errorUpload || "Error with address."}
+                </Text>
+              ) : isUploading ? (
+                <Box css={{ mt: "$4" }}>
+                  <Spinner />
+                </Box>
+              ) : attestationId ? (
+                <a
+                  target="_blank"
+                  href={`https://experiment.lvpr.tv/?v=${attestationId}`}
+                  rel="noreferrer"
+                >
+                  <Button css={{ mt: "$2" }} variant="primary" size={2}>
+                    View Uploaded Video
+                  </Button>
+                </a>
+              ) : (
+                <></>
+              )}
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Main;
